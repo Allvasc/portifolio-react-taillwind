84aa808251162f5117cb8be457d40f111155c7a0
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-05-02 13:53:10 -0300

    Adição favicon e title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,10 +33,10 @@ const Navbar = () => {
             {/* Mobile menu */}
             <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center text-center'}>
                 <li className='py-4 text-4xl'><Link onClick={handleClick} activeClass="active" to="home" smooth={true} duration={500}>Home</Link></li>
-                <li className='py-4 text-4xl'><Link onClick={handleClick}  activeClass="active" to="about" smooth={true} duration={500}>About</Link></li>
-                <li className='py-4 text-4xl'><Link onClick={handleClick}  activeClass="active" to="skills" smooth={true} duration={500}>Skills</Link></li>
-                <li className='py-4 text-4xl'><Link onClick={handleClick}  activeClass="active" to="work" smooth={true} duration={500}>Work</Link></li>
-                <li className='py-4 text-4xl'><Link onClick={handleClick}  activeClass="active" to="contact" smooth={true} duration={500}>Contact</Link></li>
+                <li className='py-4 text-4xl'><Link onClick={handleClick} activeClass="active" to="about" smooth={true} duration={500}>About</Link></li>
+                <li className='py-4 text-4xl'><Link onClick={handleClick} activeClass="active" to="skills" smooth={true} duration={500}>Skills</Link></li>
+                <li className='py-4 text-4xl'><Link onClick={handleClick} activeClass="active" to="work" smooth={true} duration={500}>Work</Link></li>
+                <li className='py-4 text-4xl'><Link onClick={handleClick} activeClass="active" to="contact" smooth={true} duration={500}>Contact</Link></li>
             </ul>
 
             {/* Social icons */}
@@ -62,11 +62,8 @@ const Navbar = () => {
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
                     </li>
-                   
                 </ul>
-
             </div>
-
         </div>
     )
 }
