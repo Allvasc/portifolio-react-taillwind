d41b38b12e6a21f9d03f1be318a3a16fa73baa51
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-05-03 19:51:59 -0300

    Correção texto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
             <form action="https://getform.io/f/33a42a21-4522-45ed-aa0b-1e287601ddc8" method='POST' className='flex flex-col max-w-[600px] w-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                    <p className='text-gray-300 py-4 text-lg sm:text-2xl'>Submit the form below or shut me an email - ah.vasconcelos@gmail.com</p>
+                    <p className='text-gray-300 py-4 text-lg sm:text-2xl'>Submit the form below or shoot me an email - ah.vasconcelos@gmail.com</p>
                 </div>
                 <input className='p-2 bg-[#ccd6f6]' type="text" name="Name" id="name" placeholder='Name' required/>
                 <input className='my-4 p-2 bg-[#ccd6f6]' type="email" name="Email" id="email" placeholder='E-mail' required/>
